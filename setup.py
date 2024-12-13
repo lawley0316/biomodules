dc877a31a830c6a0711ccc89ac0b0fec33400692
lawley0316 <lawley0316@gmail.com>
2024-12-13 15:06:40 +0800

    fastq2fasta

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/lawley0316/biomodules'
 EMAIL = 'lawley0316@gmail.com'
 AUTHOR = 'Lawley'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [

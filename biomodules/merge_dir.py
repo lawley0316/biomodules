7a07720a2df2c0761b4a84d639ccb6a2ff599df6
lawley0316 <lawley0316@gmail.com>
2024-12-13 11:07:13 +0800

    删除MergeGzipDir、添加Fastqc

--- biomodules/merge_dir.py
+++ biomodules/merge_dir.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import gzip
 from pathlib import Path
 from shutil import copyfileobj
 
@@ -27,24 +26,4 @@ class MergeDir(Module):
                 if not infile.is_file():
                     continue
                 with open(infile, 'rb') as ifp:
-                    copyfileobj(ifp, ofp)
-
-
-class MergeGzipDir(Module):
-    """给定一个输入文件夹和一个输出文件，把该文件夹下的所有gzip文件合并到该文件中。
-
-    Args:
-        indir: <pathlib.Path>，输入文件夹
-        outfile: <pathlib.Path>，输出文件（gzip文件，以.gz结尾）
-    """
-    def __init__(self, indir, outfile):
-        self.indir = indir
-        self.outfile = outfile
-
-    def main(self):
-        with gzip.open(self.outfile, 'wb') as ofp:
-            for infile in self.indir.iterdir():
-                if (not infile.is_file()) or (infile.suffix != '.gz'):
-                    continue
-                with gzip.open(infile, 'rb') as ifp:
                     copyfileobj(ifp, ofp)
